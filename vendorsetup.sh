1f3d4e3f72f9dcbb38c72e8e6c1fb715c4c7331f
Hritwik <Hritwiksinghal@outlook.in>
2021-12-03 12:08:24 +0530

    add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+export HAVOC_BUILD_TYPE=Official
+export USE_CCACHE=1
+export SKIP_ABI_CHECKS=true
+
+git -C ./external/selinux remote add can https://github.com/CannedOS/external_selinux
+git -C ./external/selinux fetch can
+git -C ./external/selinux cherry-pick db56d38c06ca4514304eec771a14558b867ab2ff
